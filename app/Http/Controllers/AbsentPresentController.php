88b0a8e756204b2116c055397307581133b85bd4
Armin Ghasemi <teamworks2016@gmail.com>
2019-12-27 20:33:33 +0100

    Creates AbsentPresent Model

---
+++ app/Http/Controllers/AbsentPresentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AbsentPresent;
+use Illuminate\Http\Request;
+
+class AbsentPresentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AbsentPresent  $absentPresent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AbsentPresent $absentPresent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AbsentPresent  $absentPresent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AbsentPresent $absentPresent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AbsentPresent  $absentPresent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AbsentPresent $absentPresent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AbsentPresent  $absentPresent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AbsentPresent $absentPresent)
+    {
+        //
+    }
+}
